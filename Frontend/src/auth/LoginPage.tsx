5f587050397524c2b97bd6db754bcf08f3c30aa6
Ivan <defalt06@mail.ru>
2026-01-04 18:58:25 +0300

    most finish auth page

--- Frontend/src/auth/LoginPage.tsx
+++ Frontend/src/auth/LoginPage.tsx
@@ -1,40 +1,52 @@
+import {motion} from "motion/react"
+
 export default function LoginPage() {
     return (
         <div className="h-screen overflow-y-auto overflow-x-hidden flex justify-center
-            items-center bg-blue-100">
-            <div className="my-12 flex flex-col">
-                <div className="flex flex-row justify-center py-16 px-12 bg-white rounded-2xl">
-                    <div className="flex flex-col justify-center items-center">
-                        <h2 className="font-medium font-sans text-2xl mb-8">
-                            Добро пожаловать
-                        </h2>
-                        <div className="flex flex-col gap-4">
-                            <input
-                                placeholder="Логин"
-                                required={true}
-                                autoFocus={true}
-                                autoComplete="username"
-                                type="text"
-                                className="w-full py-4 px-8 rounded-2xl border-2 border-gray-300
-                                     outline-none
-                                     focus:outline-none
-                                     focus:border-blue-300
-                                     focus:ring-0
-                                     transition-colors"
-                            />
-                            <input
-                                placeholder="Пароль"
-                                required={true}
-                                autoComplete="current-password"
-                                type="password"
-                                className="w-full py-4 px-8 rounded-2xl border-2 border-gray-300
-                                     outline-none
-                                     focus:outline-none
-                                     focus:border-blue-300
-                                     focus:ring-0
-                                     transition-colors"
-                            />
-                        </div>
+            items-center bg-blue-100 p-4">
+            <div className="w-full max-w-md min-w-100"> {/* Контейнер с ограничениями */}
+                <div className="flex flex-col items-center py-16 px-12 bg-white rounded-2xl w-full">
+                    <h2 className="font-medium font-sans text-3xl mb-8">
+                        Добро пожаловать
+                    </h2>
+                    <div className="flex flex-col gap-4 w-full justify-center items-center">
+                        <input
+                            placeholder="Логин"
+                            required={true}
+                            autoFocus={true}
+                            autoComplete="username"
+                            type="text"
+                            className="w-full py-3 px-8 rounded-2xl border-2 border-gray-300
+                                 outline-none
+                                 bg-gray-50
+                                 focus:outline-none
+                                 focus:border-blue-300
+                                 focus:ring-0
+                                 focus:bg-blue-50
+                                 transition-colors"
+                        />
+                        <input
+                            placeholder="Пароль"
+                            required={true}
+                            autoComplete="current-password"
+                            type="password"
+                            className="w-full py-3 px-8 rounded-2xl border-2 border-gray-300
+                                 outline-none
+                                 bg-gray-50
+                                 focus:outline-none
+                                 focus:border-blue-300
+                                 focus:ring-0
+                                 focus:bg-blue-50
+                                 transition-colors"
+                        />
+                        <motion.button className=""
+                                       initial={{width: "100%", color: "oklch(80.9% 0.105 251.813)"}}
+                                       whileHover={{width: "80%", color: "oklch(62.3% 0.214 259.815)"}}
+                                       transition={{duration: 0.4}}
+
+                        >
+                            <button className="w-full py-3 rounded-2xl text-white bg-blue-300">Войти</button>
+                        </motion.button>
 
                     </div>
                 </div>
