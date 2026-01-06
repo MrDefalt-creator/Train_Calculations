798f9ef0eecd9e5dd1f8bd5c67a697e1494298bd
Ivan <defalt06@mail.ru>
2026-01-06 12:26:22 +0300

    start backend development

---
+++ Backend/api/endpoints/user.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+from schemas.user import Login
+router = APIRouter(prefix="/users", tags=["users"])
+
+@router.post("/login")
+async def login(data: Login):
+    return data
