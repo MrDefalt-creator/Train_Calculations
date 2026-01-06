9ef994c74fc4fdba38f977a5f445c75ead3bd203
Ivan <defalt06@mail.ru>
2026-01-06 18:22:10 +0300

    remove unused imports and variables

--- Backend/database/database.py
+++ Backend/database/database.py
@@ -1,14 +1,11 @@
-from sqlalchemy import create_engine, text, Connection, MetaData
-from sqlalchemy.orm import Session, relationship
+from sqlalchemy import create_engine
 from core.config import settings
 
 
-engine =  create_engine(
+engine = create_engine(
     settings.database_url,
     echo=True,
     pool_size=5,
     max_overflow=10
 )
 
-
-metadata = MetaData()
