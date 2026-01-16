ed468c25982ce9709ddb244d99723dbd37339814
Ivan <defalt06@mail.ru>
2026-01-16 13:20:23 +0300

    Fixes

--- Backend/core/config.py
+++ Backend/core/config.py
@@ -1,23 +1,25 @@
 from typing import Optional
-
+from dotenv import load_dotenv
+import os
 from pydantic_settings import BaseSettings
 
+load_dotenv()
 
 class Settings(BaseSettings):
     
-    DATABASE_URL: str = "postgresql+psycopg://user:password@localhost:5433/train_db"
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
-    DB_HOST: Optional[str] = "localhost"
-    DB_PORT: Optional[str] = "5433"
-    DB_USER: Optional[str] = "postgres"
-    DB_PASSWORD: Optional[str] = "root"
-    DB_NAME: Optional[str] = "train_db"
+    DB_HOST: Optional[str] = os.getenv("DB_HOST")
+    DB_PORT: Optional[str] = os.getenv("DB_PORT")
+    DB_USER: Optional[str] = os.getenv("DB_USER")
+    DB_PASSWORD: Optional[str] = os.getenv("DB_PASSWORD")
+    DB_NAME: Optional[str] = os.getenv("DB_NAME")
 
-    SECRET_KEY_ACCESS: Optional[str] = "170b45bc27972f7ddbf40919a2f747712370c1747f39246e95178e06d2a6cbd1"
-    SECRET_KEY_REFRESH: Optional[str] = "b593e308abc68289966041037a603700f2d5ba466095c66b07085692de2efb2c"
-    ALGORITHM: Optional[str] = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: Optional[int] = 30
-    REFRESH_TOKEN_EXPIRE_DAYS: Optional[int] = 30
+    SECRET_KEY_ACCESS: Optional[str] = os.getenv("SECRET_KEY_ACCESS")
+    SECRET_KEY_REFRESH: Optional[str] = os.getenv("SECRET_KEY_REFRESH")
+    ALGORITHM: Optional[str] = os.getenv("ALGORITHM")
+    ACCESS_TOKEN_EXPIRE_MINUTES: Optional[int] = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+    REFRESH_TOKEN_EXPIRE_DAYS: Optional[int] = os.getenv("REFRESH_TOKEN_EXPIRE_DAYS")
 
     @property
     def database_url(self) -> str:
